94bf957e8baf24f2c0449013df2b405315d5e5fd
TONY65391 <tonyanthony65391@gmail.com>
2025-08-21 21:43:56 +0100

    first and only

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.contrib import messages
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(request = request))
