8cc3a0a0563f2b02a7436f477e5b1951b1a6818b
TONY65391 <tonyanthony65391@gmail.com>
2025-08-23 02:41:05 +0100

    almost

--- static/index.js
+++ static/index.js
@@ -2,12 +2,18 @@ const header = document.querySelector('header');
 const menuBtn = header.querySelector('nav .menu-btn');
 const menu = header.querySelector('nav .menu');
 const closeBtn = menu.querySelector('.close-nav .close-btn');
+const main = document.querySelector('main');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.remove('close');
-    menu.classList.add('open')
+    menu.classList.add('open');
+    main.classList.remove('undim')
+    main.classList.add('dim');
+    window.scrollY = 0;
 })
 closeBtn.addEventListener('click', () => {
     menu.classList.remove('open');
     menu.classList.add('close');
+    main.classList.remove('dim');
+    main.classList.add('undim')
 })
